efe36eebbdc93662d555a6284193d49b13df042f
Andrii <117983189+illusion4044@users.noreply.github.com>
2024-07-27 15:09:15 +0300

    files updated

---
+++ src/reduxTodo/todoSlice.js
@@ -0,0 +1,19 @@
+import { createSlice }  from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+    name: 'todos',
+    initialState:{
+        items:[],
+    },
+    reducers:{
+        addTodo: (state, action) => {
+            state.items = [...state.items, action.payload]
+        },
+        deleteTodo: (state, action) => {
+            state.items = state.items.filter(todo => todo.id !== action.payload);
+        },
+    }
+})
+
+export default todoSlice.reducer;
+export const {addTodo, deleteTodo} = todoSlice.actions;
